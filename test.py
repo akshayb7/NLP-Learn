f214d894ac2291cf6febdee3e14c7cf59e989d02
akshay <akshay10.bhardwaj@gmail.com>
2019-07-01 16:34:02 +0530

    initial

---
+++ test.py
@@ -0,0 +1,10 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+text = "Hello Mr. Bhardwaj! How do you do? What kind of music do you enjoy? I'm in love with Steven Wilson. XOXO"
+print(sent_tokenize(text))
+print("")
+print(word_tokenize(text))
+print("")
+for i in word_tokenize(text):
+    print(i)
+
